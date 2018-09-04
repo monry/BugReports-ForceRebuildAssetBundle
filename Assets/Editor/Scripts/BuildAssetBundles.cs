f1d01ec70286c6b9979e01dcb9bf99ab37a453a7
Tetsuya Mori <monry84@gmail.com>
2018-09-04 15:49:54 +0900

    :baby: Initial commit

---
+++ Assets/Editor/Scripts/BuildAssetBundles.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public static class BuildAssetBundles
+{
+    [MenuItem("Project/Build AssetBundles")]
+    public static void Run()
+    {
+        BuildPipeline.BuildAssetBundles(
+            "Assets/AssetBundles/Standalone",
+            BuildAssetBundleOptions.ForceRebuildAssetBundle,
+            BuildTarget.StandaloneOSX
+        );
+    }
+}
